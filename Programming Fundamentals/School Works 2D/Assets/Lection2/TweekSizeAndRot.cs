c2491012b87d52d9a071ea260e0269e5f656a1a1
Ajdin Talic <89469945+MagmarRager@users.noreply.github.com>
2022-10-05 09:07:31 +0200

    Cleaned Lec 2
    Lecture 2 simple cleaning

--- Programming Fundamentals/School Works 2D/Assets/Lection2/TweekSizeAndRot.cs
+++ Programming Fundamentals/School Works 2D/Assets/Lection2/TweekSizeAndRot.cs
@@ -4,69 +4,61 @@ using UnityEngine;
 
 public class TweekSizeAndRot : MonoBehaviour
 {
-    public GameObject laserPrefab;
 
-    [Header("Position")]
-    public float x;
-    public float y;
-    [Header("Size")]
+    // Outplayed in SampleScene
+
+    [Header("Start Size")]
     public float size = 0.5f;
-    [Header("Rotation")]
-    public float rotation; //Note: due to 2d, only in the z axis
     [Header("Misc")]
     public float moveSpeed;
     public float rotSpeed;
     public float sizeSpeed;
 
-
-
-
-
+    [Header("Component")]
+    public GameObject laserPrefab;
 
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Instantiate(laserPrefab, transform.position, transform.rotation);
         }
 
+        Movement();
 
-        // Movement x and y
+        Resize();
+
+        RotateToMouse();
+    }
+
+    void Movement()
+    {
         float xMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
         float yMove = Input.GetAxisRaw("Vertical") * moveSpeed;
 
         Vector3 move = new Vector2(xMove, yMove).normalized;
 
         transform.position += move * Time.deltaTime * moveSpeed;
+    }
 
 
-
-        // Resize with scroll
+    void Resize()
+    {
         float scrollSize = Input.GetAxis("Mouse ScrollWheel");
 
         transform.localScale += Vector3.one * size * scrollSize;
+    }
 
 
-
-        // Rotate towoards mouse
+    void RotateToMouse()
+    {
         Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotDirection = Quaternion.AngleAxis(angle, Vector3.forward);
 
 
         transform.rotation = Quaternion.Lerp(transform.rotation, rotDirection, Time.deltaTime * rotSpeed);
-
-
-
-
-        // rotate Robert version
-        //Vector2 aim = Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition;
-        //transform.up = aim;
-
-
     }
-
-
 
 }
